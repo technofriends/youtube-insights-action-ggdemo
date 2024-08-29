d2c5d684d23e4ea10b7c7836e9e72d193092eae5
Vaibhav Pandey <pandey.vaibhav@gmail.com>
2024-08-29 14:37:44 +0530

    fixing render deployment.

--- app/main.py
+++ app/main.py
@@ -2,16 +2,12 @@ from flask import Flask
 from dotenv import load_dotenv
 from app.routes.webhook import webhook
 
-# Load environment variables
 load_dotenv()
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-# Initialize Flask app
 app = Flask(__name__)
-
-# Register blueprints
 app.register_blueprint(webhook)
 
 if __name__ == '__main__':
